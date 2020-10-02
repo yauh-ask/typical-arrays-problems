f228ac27c1c936446dfe2dc39175148fcbcd61b3
yauh-ask <yauheniya.askolkava@gmail.com>
2020-10-02 14:12:21 +0200

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-
 exports.min = function min (array) {
-  return 0;
+    return !Array.isArray(array) || array.length == 0 ? 0 : Math.min(...array)
 }
 
 exports.max = function max (array) {
-  return 0;
+    return !Array.isArray(array) || array.length == 0 ? 0 : Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return !Array.isArray(array) || array.length == 0
+        ? 0
+        : array.reduce((a, b) => a + b) / array.length
 }
